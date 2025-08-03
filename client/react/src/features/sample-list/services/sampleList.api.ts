e5bbc650ae9576afffd7691e32475bb8c8794327
mauroj <mauroj@siainteractive.com>
2025-08-03 13:47:38 +0200

    feat: add json-server mock-api

--- client/react/src/features/sample-list/services/sampleList.api.ts
+++ client/react/src/features/sample-list/services/sampleList.api.ts
@@ -1,7 +1,7 @@
 import type { Sample } from '../types';
 
 export const getSamples = async (): Promise<Sample[]> => {
-  const res = await fetch('http://localhost:4000/api/samples');
+  const res = await fetch('http://localhost:3001/samples');
   if (!res.ok) throw new Error('No se pudo obtener samples');
   return res.json();
 };
